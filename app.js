9fcdc9f58dc17e6858e5ae044141e96300d93fcb
madison <mdsncole97@gmail.com>
2023-10-14 09:29:02 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+//User input
+const todaysSales = document.querySelector("#todays-sales");
+const planSales = document.querySelector("#plan-sales");
+const tySales = document.querySelector("#ty-sales");
+const lySales = document.querySelector("#ly-sales");
+const tyTraffic = document.querySelector("#ty-traffic");
+const lyTraffic = document.querySelector("#ly-traffic");
+
+//Calculator submit buttons
+const planCalc = document.querySelector("#plan-calc");
+const lyCalc = document.querySelector("#ly-calc");
+const spreadCalc = document.querySelector("#spread-calc");
+
+
+
+
+
+// planCalc.addEventListener("click", function () {
+//   return (todaysSales / planSales) * 100 - 100
+// });
+
+
+
+
+
+//% difference of this years traffic from last years traffic
+// const trafficPercent = (tyTraffic, lyTraffic) => (
+//   (tyTraffic / lyTraffic) * 100 - 100
+// );
+
+// //% difference from this years sales to last years sales
+// const salesPercent = (tySales, lySales) => (
+//   (tySales / lySales) * 100 - 100
+// );
+
+// //finding spread from TY/LY sales to TY/LY traffic
+// const spread = (salesPercent, trafficPercent) => (
+//   salesPercent - trafficPercent
+// );
+
+// Testing
+// console.log(trafficPercent(123, 213));
